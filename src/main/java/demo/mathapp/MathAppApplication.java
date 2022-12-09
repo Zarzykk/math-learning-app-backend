78c00c54fdec0b331cc6e8b2320d8478ddee0b0d
Zarzyk <sebekzarzyk@gmail.com>
2022-12-09 12:26:41 +0100

    Initial commit

---
+++ src/main/java/demo/mathapp/MathAppApplication.java
@@ -0,0 +1,23 @@
+package demo.mathapp;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class MathAppApplication {
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/mathapp/MathAppApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.mathapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
